3e91a1d8b4b1eb97faa9a23f4cc46f07e00fa938
filipmilivojevic <fmilivojevic01@bvsd.org>
2026-01-31 20:47:44 +0000

    Completed the basics like reading the file and printing the Size of it. I need to work on all of my methods then hook it up to the GUI

--- benGymApp.java
+++ benGymApp.java
@@ -1,1 +1,49 @@
+import java.util.Scanner; //for reading keyboard input and Files
+import java.io.File;
+import java.io.PrintWriter; //for printing to a file
+import java.io.IOException; //Exception that can be thrown by File and Scanner
+import java.util.ArrayList;
 
+public class benGymApp{
+
+public static ArrayList<String[]> readDataFromFile(String filename){
+    ArrayList<String[]> benExercises = new ArrayList<>();
+   try{
+     File file = new File(filename);
+    Scanner scan = new Scanner(file);
+
+    while (scan.hasNextLine()){
+        String line = scan.nextLine();
+        String[] row = line.split(",");
+        benExercises.add(row);
+    }
+    scan.close();
+    }
+    catch (Exception e){
+        System.out.println("Cannot Read file");
+    }
+    
+    return benExercises;
+}
+
+
+
+
+
+
+// Main Method
+
+public static void main(String[] args){
+    Scanner keyboard = new Scanner(System.in);
+    String inputfile = keyboard.nextLine();
+    ArrayList<String[]> exerciseData  = readDataFromFile(inputfile);
+    System.out.println(exerciseData.size());
+}
+
+
+
+
+
+
+
+}
