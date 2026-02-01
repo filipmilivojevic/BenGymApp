96e3bf8030b6db6d6570367bddbdbf7e75638255
filipmilivojevic <fmilivojevic01@bvsd.org>
2026-02-01 16:41:04 +0000

    added all exercises to different list categories

--- benGymApp.java
+++ benGymApp.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 
 public class benGymApp{
 
+
+
+
+
+
+
 public static ArrayList<String[]> readDataFromFile(String filename){
     ArrayList<String[]> benExercises = new ArrayList<>();
    try{
@@ -26,6 +32,41 @@ public static ArrayList<String[]> readDataFromFile(String filename){
     return benExercises;
 }
 
+public static void addCategories(ArrayList<String[]> exerciseData, ArrayList<String[]> pushExercises, ArrayList<String[]> pullExercises, ArrayList<String[]> legsExercises, ArrayList<String[]> otherExercises){
+    for (String[] row : exerciseData){
+        String exerciseNum = row[2];
+        if (exerciseNum.equals("chest"))
+            pushExercises.add(row);
+        if (exerciseNum.equals("triceps"))
+            pushExercises.add(row);
+        if (exerciseNum.equals("shoulders"))
+            pushExercises.add(row);
+        if (exerciseNum.equals("back"))
+            pullExercises.add(row);
+        if (exerciseNum.equals("biceps"))
+            pullExercises.add(row);
+        if (exerciseNum.equals("arms"))
+            pullExercises.add(row);
+        if (exerciseNum.equals("legs"))
+            legsExercises.add(row);
+        if (exerciseNum.equals("abs"))
+            otherExercises.add(row);
+
+    }}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -37,7 +78,17 @@ public static void main(String[] args){
     Scanner keyboard = new Scanner(System.in);
     String inputfile = keyboard.nextLine();
     ArrayList<String[]> exerciseData  = readDataFromFile(inputfile);
-    System.out.println(exerciseData.size());
+    ArrayList<String[]> pushExercises = new ArrayList<>();
+    ArrayList<String[]> pullExercises = new ArrayList<>();
+    ArrayList<String[]> legsExercises = new ArrayList<>();
+    ArrayList<String[]> otherExercises = new ArrayList<>();
+    addCategories(exerciseData, pushExercises, pullExercises, legsExercises, otherExercises);
+
+
+    
+    for ( String[] item : pushExercises){
+        System.out.println(java.util.Arrays.toString(item));
+    }
 }
 
 
