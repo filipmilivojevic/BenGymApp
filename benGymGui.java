38261a84a8d45930c70da8577d78303552721e85
filip <filip2009milivojevic@gmail.com>
2026-03-01 23:07:18 -0700

    Creation with the GUI

--- benGymGui.java
+++ benGymGui.java
@@ -7,5 +7,22 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class benGymGui implements ActionListener {
+    JFrame mainFrame;
+    JPanel mainPanel;
+    JCheckBox[] selectionBoxes;
+    JButton displayResultsButton;
+    JTextField calorieEntry;
+    JTextField stressEntry;
+    JTextPane resultText;
 
-}
+
+public benGymGui(){
+    mainFrame = new JFrame("Ben's Gym APP");
+    mainPanel = new JPanel();
+    selectionBoxes = buildCheckBoxes();
+    displayResultsButton = new JButton("Calculate");
+    displayResultsButton.addActionListener(this);
+    userEntry = new JTextField();
+    resultText = new JTextPane();
+    resultText.setContentType("text/html");
+}}
